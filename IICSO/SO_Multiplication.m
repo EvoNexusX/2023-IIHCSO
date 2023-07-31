4f8e9388046e6f9a17f0ea0fdac9fdfbeb6a48d9
EvoNexusX <139948124+EvoNexusX@users.noreply.github.com>
2023-07-31 10:24:11 +0800

    Delete IICSO directory

--- IICSO/SO_Multiplication.m
+++
@@ -1,6 +0,0 @@
-function SO = SO_Multiplication(c,SO1)
-    c = max(1,floor(10*c));
-    c = min(c,length(SO1));
-    SO = SO1(1:c);
-end
-
--- IICSO/main.m
+++
@@ -1,4 +0,0 @@
-clc;
-clear;
-%Example of IICSO usage
-num_HCSO(1,1) = IICSO(num2str(25),25,500)
