b65687e8920add27f5ce26c1c29c8e92d8b025d0
EvoNexusX <139948124+EvoNexusX@users.noreply.github.com>
2024-01-20 12:57:48 +0800

    Delete IIHCSO directory

--- IIHCSO/Vis_Bridge_Coast.m
+++
@@ -1,5 +0,0 @@
-function Vis = Vis_Bridge_Coast(VisBridgeCoast,Left,Right,c)
-    Vis = VisBridgeCoast;
-    Vis(Left:Right) = c;
-end
-
--- IIHCSO/calcuate_hierarchy_prob.m
+++
@@ -1,5 +0,0 @@
-function hierarchy_prob = calcuate_hierarchy_prob(hierarchy)
-    j = 1:hierarchy;
-    hierarchy_prob = (hierarchy-j + 1)./sum(j);
-end
-
